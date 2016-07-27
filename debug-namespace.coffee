9d48ea73ed7f78500ef8697fd3da8b4f3e7ae8d4
anirudhb <anirudhqazxswced+github@gmail.com>
2016-07-27 11:42:40 -0700

    Coffeescript

---
+++ debug-namespace.coffee
@@ -0,0 +1,10 @@
+###
+ * debug-namespace
+ * Package for namespacing easily in the debug module
+ * Licensed under the GPL
+###
+
+###
+ * Changelog
+ * 0.0.1 Coffeescript
+###
---
+++ debug-namespace.js
@@ -0,0 +1,12 @@
+
+/*
+ * debug-namespace
+ * Package for namespacing easily in the debug module
+ * Licensed under the GPL
+ */
+
+/*
+ * Changelog
+ * 0.0.1 Coffeescript
+ */
+
